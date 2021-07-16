ed4263e7d47d1aeeef1b56388859ca7258d96cf3
lenovo <faroukayadi1@gmail.com>
2021-07-16 16:27:42 +0200

    add listView of Favorited screen

--- lib/screens/Home_tab/Favorited.dart
+++ lib/screens/Home_tab/Favorited.dart
@@ -1,3 +1,6 @@
+import 'dart:html';
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 
 class Favorite extends StatefulWidget {
@@ -8,10 +11,70 @@ class Favorite extends StatefulWidget {
 }
 
 class _FavoriteState extends State<Favorite> {
+  List<MaterialColor> textcolor = [
+    Colors.deepOrange,
+    Colors.teal,
+    Colors.cyan,
+    Colors.brown,
+  ];
+
+Random random = Random();
+  MaterialColor _getRandomColor(){
+    return textcolor[random.nextInt(textcolor.length)];
+  }
   @override
   Widget build(BuildContext context) {
-    return Container(
-      color: Colors.green,
+    return ListView.builder(itemBuilder: (BuildContext context,int index){
+      return Column(
+        children: [
+          _drawAuthorRow(Colors.deepOrange),
+          _drawItemRow(),
+        ],
+      );
+    },
+    itemCount: 20,);
+  }
+  Widget _drawAuthorRow(MaterialColor textcolor){
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      children: [
+        Row(
+          children: [
+            Container(
+              decoration: BoxDecoration(
+                shape: BoxShape.circle,
+                  image:DecorationImage(
+                  image: ExactAssetImage('assets/images/back.jpg'),
+                   fit: BoxFit.cover
+                ),
+              ),
+           width: 40,
+              height: 20,
+            ),
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text('Michael Adams',style: TextStyle(color: Colors.grey),),
+                Row(
+                  children: [
+                    Text('15 min',style: TextStyle(color: Colors.grey)),
+                    Icon(Icons.visibility),
+                    Text('Lifestyle',style: TextStyle(color: _getRandomColor()),),
+                  ],
+                )
+              ],
+            )
+          ],
+        ),
+        IconButton(
+          icon: const Icon(Icons.bookmark_border),
+          color: Colors.grey,
+          onPressed: () {},
+        ),
+      ],
     );
+  }
+  Widget _drawItemRow(){
+    return Container();
   }
 }
