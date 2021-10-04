126c6225efa72c4d5a0cbe5f3d8fa64471f8b8ee
lenovo <faroukayadi1@gmail.com>
2021-10-04 20:14:57 +0100

    Add sliver to SinglePost Page

--- lib/screens/SinglePost.dart
+++ lib/screens/SinglePost.dart
@@ -1,3 +1,5 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 import 'package:news_application/Models/Post.dart';
 class SinglePost extends StatefulWidget {
@@ -13,11 +15,48 @@ class _SinglePostState extends State<SinglePost> {
   late final Post post;
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(),
-      body: Container(
-        color: Colors.orange,
-      )
+    return CustomScrollView(
+      slivers: [
+        SliverAppBar(
+          expandedHeight: MediaQuery.of(context).size.height / 2.5,
+          floating: true,
+          pinned: true,
+          flexibleSpace: FlexibleSpaceBar(
+            background: Container(
+              decoration: BoxDecoration(
+                image: DecorationImage(
+                  image: NetworkImage(widget.post.featured_image), fit: BoxFit.fill
+                )
+              ),
+            ),
+          ),
+        ),
+        SliverList(
+            delegate: SliverChildBuilderDelegate(
+                (context,position){
+                  return Container(
+                  height: 150,
+                    color: generateRandomColor1(),
+                  );
+                },
+                childCount : 25,
+            ),
+        )
+      ],
+
+
     );
+  }
+  Color generateRandomColor1() {
+    // Define all colors you want here
+    const predefinedColors = [
+      Colors.red,
+      Colors.green,
+      Colors.blue,
+      Colors.black,
+      Colors.white
+    ];
+    Random random = Random();
+    return predefinedColors[random.nextInt(predefinedColors.length)];
   }
 }
